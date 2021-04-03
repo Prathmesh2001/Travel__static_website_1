49aa799c41232e8d56522a3a3838634021b85393
PatilOjas <ojaspatil1901@gmail.com>
2021-04-03 19:56:42 +0530

    Backend

--- Travelsite/Travelsite/urls.py
+++ Travelsite/Travelsite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from traveller.views import about_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', about_page)
 ]
